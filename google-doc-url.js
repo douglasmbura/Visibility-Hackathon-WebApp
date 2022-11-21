e31663375063753bc2efc64fab4966c0a38ab954
douglasmbura <douglas.mbura@gmail.com>
2022-11-21 23:05:48 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/11VYnIz8YSabM5ZYOTRxm_RcDkle0GM5hScj22YyLnug/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyCA74eO7WQtYZxlYPCb0UNWZfnopHUhlgg';
+//var googleApiKey = 'AIzaSyCA74eO7WQtYZxlYPCb0UNWZfnopHUhlgg';
